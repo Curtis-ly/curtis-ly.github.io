7d73f8d6792f98a47e901381520c1f7654f8f6d6
DESKTOP-UQ0JRPT\Curtis <liuyang8903@163.com>
2018-12-02 21:37:13 +0800

    界面更新

---
+++ precache-manifest.baa186661760f22ea22da6b4786b8636.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "af21e208fcbf36f45ce2",
+    "url": "./static/js/main.af21e208.chunk.js"
+  },
+  {
+    "revision": "bee79b8a4f638cea908f",
+    "url": "./static/js/1.bee79b8a.chunk.js"
+  },
+  {
+    "revision": "af21e208fcbf36f45ce2",
+    "url": "./static/css/main.378b75c4.chunk.css"
+  },
+  {
+    "revision": "bee79b8a4f638cea908f",
+    "url": "./static/css/1.e8708307.chunk.css"
+  },
+  {
+    "revision": "b6acc784a266cf5397c0f374e4ae4e6c",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d27082fe6891bc4f146234a5c23b66f2.js"
+  "./precache-manifest.baa186661760f22ea22da6b4786b8636.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
